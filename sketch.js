b3d794148fdad083a8ddf30616d2e5fd09a4c2a7
rxg962 <rxg962@gmail.com>
2026-01-10 21:04:42 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ async function setup() {
   if (windowWidth < gameWidth) {
     gameWidth = windowWidth;
   }
-  createCanvas(gameWidth, windowHeight - 75);
+  createCanvas(gameWidth, windowHeight);
 
   pixelDensity(window.devicePixelRatio);
 
